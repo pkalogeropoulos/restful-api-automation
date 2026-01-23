518c31025767d5de6310b55639ea33030b0eac43
p.kalogeropoulos@xe.gr <p.kalogeropoulos@xe.gr>
2026-01-23 14:06:06 +0200

    Added more infra in TestDataFactory

--- src/test/java/dev/restfulApi/base/TestDataFactory.java
+++ src/test/java/dev/restfulApi/base/TestDataFactory.java
@@ -1,2 +1,31 @@
-package dev.restfulApi.base;public class TestDataFactory {
+package dev.restfulApi.base;
+
+import dev.restfulApi.client.ApiClient;
+import dev.restfulApi.dtos.ObjectRequest;
+import dev.restfulApi.dtos.ObjectResponse;
+import io.restassured.response.Response;
+
+import java.util.Map;
+
+public final class TestDataFactory {
+
+    private TestDataFactory() {
+    }
+
+    public static ObjectResponse createObject(ApiClient api, ObjectRequest req) {
+        Response res = api.createObject(req);
+
+        ObjectResponse created = res.as(ObjectResponse.class);
+        return created;
+    }
+
+    public static ObjectRequest requestWithName(String name) {
+        return ObjectRequest.builder()
+                .name(name)
+                .data(Map.of("k", "v")).build();
+    }
+
+    public static String definitelyMissingId() {
+        return "999999999999";
+    }
 }
